1dcab8536ae54683dae2722d7749b8624aca6ca1
lapislazuli0929 <100329159+lapislazuli0929@users.noreply.github.com>
2022-03-15 21:12:49 +0800

    Add files via upload

---
+++ homework.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<center><font size="10">墾丁三日遊報名表</font></center>
+</head>
+
+<body>
+<body bgcolor="#cccccc">
+<center>1.行程內容<br>
+墾丁國家公園是台灣在台灣戰後時期第一個成立的國家公園，<br>
+成立於1982年，隸屬內政部營建署，下設管理處。<br>
+全境位於台灣島南端的恆春半島，<br>
+範圍包括屏東縣恆春鎮、滿州鄉、車城鄉部分陸域及周邊海域，以墾丁里名之。<br>
+<img src="Nanwan_(South_Bay).jpg" width="550px" height="420px">
+
+<center><a href="https://mimihan.tw/kenting-travel/">這個連結</a>會連到介紹墾丁的景點！！<br></center>
+<br></center>
+<br><br>
+
+
+<FORM ACTION"" METHOD"">
+2.<br>
+Name:<input type="text"name="name" value="name"> <br>
+Email:<input type="text" name="email" value="email"> <br>
+Tel:<input type="text"name="ntel" value="tel"> <br>
+Gender:<input type="radio"name="gender" value="male"> Male 
+<input type="radio"name="gender" value="female"> Female<br>
+Food refernces:<br>
+<input type="checkbox" name="food" value="cr">中式<br>
+<input type="checkbox" name="food" value="wr">西式<br>
+<input type="checkbox" name="food" value="jr">日式<br>
+T-Sirt Size:
+<select>
+    <option>S</option>
+    <option>M</option>
+    <option>L</option>
+    <option>XL</option>
+</select>
+<br>
+
+T-Shirt color:<input type="color" value="#ff0000"><br>
+Birthday:<input type="date"><br>
+Ticket: <input type ="number"> <br>
+<input type="submit"> 
+
+</FORM>
+
+
+<center><font size="4">T-shirt size 對照表</font></center>
+<center><TABLE border="6">
+<TR> <TH>   <TH>S       <TH>M       <TH>L       <TH>XL     </TR> 
+<TR> <TH>長 <TH>26"/66  <TH>27"/68  <TH>28'/71  <TH>29"/74 </TR>  
+<TR> <TH>寬 <TH>17"/43  <TH>18"/46  <TH>19"48   <TH>20"/51 </TR> 
+<TR> <TH>高 <TH>160-168 <TH>165-173 <TH>170-178 <TH>175-183</TR>
+</TABLE></center>
+
+</body>
+
+</html>
